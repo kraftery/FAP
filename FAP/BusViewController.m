c749409c0aba628b4c4042cca889962a4a482c63
Kieran Raftery <kraftery7@gmail.com>
2013-11-23 18:23:09 -0500

    add busviewcontroller

---
+++ FAP/BusViewController.m
@@ -0,0 +1,38 @@
+//
+//  BusViewController.m
+//  FAP
+//
+//  Created by Kieran Raftery on 11/23/13.
+//  Copyright (c) 2013 Mobile App Developers Club. All rights reserved.
+//
+
+#import "BusViewController.h"
+
+@interface BusViewController ()
+
+@end
+
+@implementation BusViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
