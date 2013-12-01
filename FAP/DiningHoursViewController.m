679f9ea4b58c050d8aed0f0f40547f8eceb40ca9
Kieran Raftery <kraftery7@gmail.com>
2013-11-30 20:54:45 -0500

    Got the food deals view ready for when we have the data

--- FAP/DiningHoursViewController.m
+++ FAP/DiningHoursViewController.m
@@ -28,13 +28,49 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
-    self.title = @"Dining Hours";
+    self.title = @"Food Deals";
+    
+    restaurants = [[NSArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+#pragma mark Table view data source
+
+-(NSInteger)numberOfSectionsOfTableView:(UITableView *) tableView {
+    //return the number of sections
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    //return the number of rows in the section
+    return [restaurants count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.backgroundColor = [UIColor clearColor];
+    cell.textLabel.text = [restaurants objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark-
+#pragma mark Table view delegate
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
 }
 
 @end
