78ae9bc32b67db357bb4f48107b9dedacd96ebd1
Kieran Raftery <kraftery7@gmail.com>
2013-12-10 10:47:49 -0500

    Added EventViewController

---
+++ FAP/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  FAP
+//
+//  Created by Kieran Raftery on 12/10/13.
+//  Copyright (c) 2013 Mobile App Developers Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+
+@end
---
+++ FAP/EventViewController.m
@@ -0,0 +1,38 @@
+//
+//  EventViewController.m
+//  FAP
+//
+//  Created by Kieran Raftery on 12/10/13.
+//  Copyright (c) 2013 Mobile App Developers Club. All rights reserved.
+//
+
+#import "EventViewController.h"
+
+@interface EventViewController ()
+
+@end
+
+@implementation EventViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
